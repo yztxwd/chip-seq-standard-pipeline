0975b91e62e3528f7fe32bed056d66ebcf0ef39d
Jianyu Yang <yztxwd@E1-053578.local>
2021-11-18 17:27:48 -0500

    Add git submodule init

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,3 +1,15 @@
 import subprocess
 
-subprocess.call(['git', 'submodule', 'update', '--init'])
+def main():
+    repo = "{{cookiecutter.git_submodule}}".strip().split("/")
+    repo_name = repo.split("/")[-1].replace(".git", "")
+
+    print(repo)
+
+    subprocess.check_call(['git', 'init'])
+    subprocess.check_call(['git', 'checkout', '-b', 'main'])
+    subprocess.check_call(['rm', '-rf', f'{repo_name}'])
+    subprocess.check_call(['git', 'submodule', 'add', f'repo'])
+
+if __name__ == '__main__':
+    main()
