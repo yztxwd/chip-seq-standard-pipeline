70abf7b7228c7793de965578fd5dc1895dd02cb1
Jianyu Yang <yztxwd@E1-053578.local>
2021-11-18 17:32:26 -0500

    Minor fix

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -9,7 +9,7 @@ def main():
     subprocess.check_call(['git', 'init'])
     subprocess.check_call(['git', 'checkout', '-b', 'main'])
     subprocess.check_call(['rm', '-rf', f'{repo_name}'])
-    subprocess.check_call(['git', 'submodule', 'add', f'repo'])
+    subprocess.check_call(['git', 'submodule', 'add', f'{repo}'])
 
 if __name__ == '__main__':
     main()
