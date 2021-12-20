4509ba1f22057f7d5b08d83c043569448ed216ed
Jianyu Yang <yztxwd@E1-053578.local>
2021-12-20 18:37:56 -0500

    Minor fix

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,9 @@
 import subprocess
 
 def main():
+    repo = "{{cookiecutter.git_submodule}}"
+    repo_name = repo.strip().split("/")[-1].replace(".git", "")
+    
     # merge config
     f = open('config.yaml', 'a')
     subprocess.check_call(['cat', f'{repo_name}/config.yaml'], stdout=f)
--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -5,8 +5,6 @@ def main():
     repo = "{{cookiecutter.git_submodule}}"
     repo_name = repo.strip().split("/")[-1].replace(".git", "")
 
-    print(repo)
-
     subprocess.check_call(['git', 'init'])
     subprocess.check_call(['git', 'checkout', '-b', 'main'])
     subprocess.check_call(['rm', '-rf', f'{repo_name}'])
