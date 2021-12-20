7731786db8f6aea1e890c21bc7856286a89c5a5f
Jianyu Yang <yztxwd@E1-053578.local>
2021-12-20 18:17:51 -0500

    Merge config

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -11,6 +11,11 @@ def main():
     subprocess.check_call(['git', 'checkout', '-b', 'main'])
     subprocess.check_call(['rm', '-rf', f'{repo_name}'])
     subprocess.check_call(['git', 'submodule', 'add', f'{repo}'])
+    
+    # merge config
+    f = open('config.yaml', 'a')
+    subprocess.check_call(['cat', f'{repo_name}/config.yaml'], stdout=f)
+    f.close()
 
 if __name__ == '__main__':
     main()
