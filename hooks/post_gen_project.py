6a5de091c55711af1c2d81e651f1fdacd5062251
yztxwd <yztxwd@gmail.com>
2023-03-02 16:41:59 -0500

    fix: add smk file in post_gen_project hook script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,10 +1,20 @@
+import os
 import shutil
 import subprocess
 
+CHILD_DIR = 'snakemake-pipeline-general/'
+
 def main():
     # concatenate config file
-    with open('snakemake-pipeline-general/config.yaml') as fi, open('config.yaml', 'a') as fo:
+    with open(f'{CHILD_DIR}/config.yaml') as fi, open('config.yaml', 'a') as fo:
         shutil.copyfileobj(fi, fo)
+    
+    # include rules into Snakefile
+    with open('Snakefile', 'a') as f:
+        for rule in os.listdir(f'{CHILD_DIR}/rules'):
+            f.write(f'include: {CHILD_DIR}/rules/{rule}\n')
+        for rule in os.listdir('./rules'):
+            f.write(f'include: ./rules/{rule}\n')
     
     # init the git
     subprocess.check_call(['git', 'init'], stdout=subprocess.DEVNULL)
