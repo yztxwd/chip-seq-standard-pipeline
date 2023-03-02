f9073b2854e0fa42b790906a31625588ef5a23a8
yztxwd <yztxwd@gmail.com>
2023-03-02 15:42:39 -0500

    clean hook scripts

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,16 +2,11 @@ import subprocess
 from cookiecutter.main import cookiecutter
 
 def main():
-    # test
     cookiecutter(
         'https://github.com/yztxwd/snakemake-pipeline-general.git',
         checkout='improve-integrateGeneralPipelineConfig',
         extra_context={'directory_name': 'snakemake-pipeline-general'}
     )
     
-
-    
-    
-
 if __name__ == '__main__':
     main()
