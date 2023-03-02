84ed6f315677d30ff6db789dee6663628179c2e8
yztxwd <yztxwd@gmail.com>
2023-03-02 15:44:14 -0500

    minor fix

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -2,6 +2,8 @@ import subprocess
 
 def main():
     subprocess.check_call(['git', 'init'])
+    subprocess.check_call(['git', 'add', '.'])
+    subprocess.check_call(['git', 'commit', '-m', '"first commit"'])
 
 if __name__ == '__main__':
     main()
