628559756552d971b739fa6e62150169da7f56f0
yztxwd <yztxwd@gmail.com>
2023-03-02 15:48:19 -0500

    minor fix

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,10 @@
 import subprocess
 
 def main():
+    # concatenate config file
+    subprocess.check_call(['cat', 'snakemake-pipeline-general/config.yaml', '>>', 'config.yaml'], shell=True)
+    
+    # init the git
     subprocess.check_call(['git', 'init'])
     subprocess.check_call(['git', 'add', '.'])
     subprocess.check_call(['git', 'commit', '-m', '"first commit"'])
