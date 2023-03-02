79be004629149b9f54c98e1a8058d0bb541dbd13
yztxwd <yztxwd@gmail.com>
2023-03-02 14:37:43 -0500

    minor fix

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -4,7 +4,7 @@ def main():
     # test
     subprocess.check_call(['git', 'init'])
     subprocess.check_call(['git', 'submodule', 'add', 'https://github.com/yztxwd/snakemake-pipeline-general'])
-    subprocess.check_call(['cat', 'snakemake-pipeline-general'])
+    subprocess.check_call(['cat', 'snakemake-pipeline-general/config.yaml'])
 
 if __name__ == '__main__':
     main()
